9599e90dd640aac044a151504e9ff15e5b5e8223
Glitchbott <36093622+Glitchbott@users.noreply.github.com>
2018-03-10 19:50:52 +0600

    The main parent class is System.Object

---
+++ ObjectOverrides/ObjectOverrides/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Fun with System.Object***\n");
+            Person p1 = new Person();
+            // У наследованные члены System
+            Console.WriteLine("ToString: {0}",p1.ToString());
+            Console.WriteLine("Hash code: {0}",p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+            //Создать другую сылку на p1
+            Person p2 = p1;
+            object o = p2;
+
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
